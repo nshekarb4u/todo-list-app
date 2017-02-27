d88d7db3fdae364fe426df4043dddf052564efd6
Shekar Nyala <nshekarb4u@gmail.com>
2017-02-27 07:47:48 -0600

    merged with todoList

--- src/main/java/com/nice/todolist/TodoListAppApplication.java
+++ src/main/java/com/nice/todolist/TodoListAppApplication.java
@@ -2,9 +2,7 @@ package com.nice.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.nice.todolist.dto.UserDto;
 
 @SpringBootApplication
 public class TodoListAppApplication {
